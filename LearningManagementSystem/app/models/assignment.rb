c2c3f62cba44b7fd59e330448d745b1676c4b676
Brandon Popson <Brandon.Popson@outlook.com>
2019-03-31 13:48:25 -0500

    Fixes assignment name being too short

--- LearningManagementSystem/app/models/assignment.rb
+++ LearningManagementSystem/app/models/assignment.rb
@@ -2,7 +2,7 @@ class Assignment < ApplicationRecord
   belongs_to :course
   has_many :grades, :dependent => :destroy
 
-  validates_length_of :assignment_name, :within => 1..20
+  validates_length_of :assignment_name, :within => 1..50
 
   def full_assignment_name
     "#{course.course_name} - #{assignment_name}"
